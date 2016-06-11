88d39aae22994bba3ddb96dc25f26de1e0efda4b
Davide Porzio <salvatore.porzio@postgrad.manchester.ac.uk>
2016-06-11 11:00:31 +0100

    auto

--- extractRates.py
+++ extractRates.py
@@ -12,15 +12,15 @@ import Modules.querySQL as QSQL
 
 # StartTime and endTime for queries
 # In order to correctly advance hour by hour, it should be annoyingly converted to a Unix timestamp (to add 3600 seconds) and then back to a datetime object
-startTime = dt(2016, 1, 1, 0, 0, 0)
-endTime = dt(2016, 1, 1, 0, 10, 0)
+startTime = dt(2015, 9, 1, 0, 0, 0)
+endTime = dt(2016, 6, 1, 0, 0, 0)
 stepSize = 24*60*60 # in seconds
 options = [startTime,endTime,stepSize]
 
 # Variable names
 varNames = np.genfromtxt(projDir+"/channelNames.dat",names=True,dtype=None)
 varPaths = []
-for name in varNames['VariableName']:
+for k,name in enumerate(varNames['VariableName']):
     # Number of steps from startTime to endTime
     diffTime = time.mktime(endTime.timetuple()) - time.mktime(startTime.timetuple())
     nSteps = int(diffTime/stepSize)
@@ -34,7 +34,7 @@ for name in varNames['VariableName']:
         rate = QSQL.GetEntriesNumberByName(varName,loopTimeLeft,loopTimeRight)
         timestampOut = DTO.GetChicagoTimestampDT(loopTimeLeft)
         dataOut.append([timestampOut,rate])
-        print name,loopTimeLeft.strftime('%Y %b %d %H:%M:%S'),rate
+        print k+1,"of",len(varNames),name,loopTimeLeft.strftime('%Y %b %d %H:%M:%S'),rate
         loopTimeLeft = loopTimeRight
 
     outPath = "Data/"+name.replace('/','_')+"_"+startTime.strftime('%y%m%d')+"_"+endTime.strftime('%y%m%d')+"_"+str(stepSize)+"s.dat"
@@ -42,7 +42,4 @@ for name in varNames['VariableName']:
     np.savetxt(outPath,np.array((dataOut)),delimiter=' ',header='Timestamp Rate')
 
 np.savetxt(projDir+'/Data/outList.dat',np.array(varPaths),delimiter=' ',fmt="%s",header='VariablePath')
-
-# print np.matrix(options)
-# print np.matrix(options).transpose()
 np.savetxt(projDir+'/Data/options.dat',np.matrix(options),delimiter='\t',fmt='%s',header='StartTime\tEndTime\tStepSize')
